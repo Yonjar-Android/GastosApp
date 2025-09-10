0efa8ec914399c11afeaadefe1ce4932def9c674
Juan <juan132y@gmail.com>
2025-09-10 09:45:28 -0600

    Add Compose Navigation dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,4 +77,6 @@ dependencies {
     implementation(libs.androidx.paging.runtime)
     testImplementation(libs.androidx.paging.common)
     implementation(libs.androidx.paging.compose)
+
+    implementation(libs.androidx.navigation.compose)
 }
